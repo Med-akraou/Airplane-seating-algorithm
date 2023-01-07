34b19ae90c1c9d72020aac9e521bc20db33c2daf
Med-akraou <medakraou21@gmail.com>
2023-01-07 17:44:50 +0100

    seating algo

---
+++ main.py
@@ -0,0 +1,98 @@
+
+seats = [[2,3],[3,3],[3,2]]
+
+def init_seats(seats):
+    #fill all seats with 0 as default value
+    seating = []
+    for i,item in enumerate(seats):
+        seating.append([])
+        for row in range(item[0]):
+            seating[i].append([])
+            for col in range(item[1]):
+                seating[i][row].append(0)
+    return seating
+
+sets_grid = init_seats(seats)
+max_rows = max([l[0] for l in seats])
+
+def fill_aisle_seats(num_passenger):
+    current_passenger = 1
+    row = 0
+    while(current_passenger<=num_passenger and row  <max_rows):
+        for i,part in enumerate(sets_grid):
+            if row < len(part) and current_passenger<=num_passenger:
+                if i==0:
+                    sets_grid[i][row][-1] = current_passenger
+                    current_passenger += 1
+                elif i==len(sets_grid)-1:
+                    sets_grid[i][row][0]=current_passenger
+                    current_passenger+=1
+                else:
+                    sets_grid[i][row][0]=current_passenger
+                    current_passenger+=1
+                    if current_passenger<=num_passenger and len(part[0])>1:
+                        sets_grid[i][row][-1]=current_passenger
+                        current_passenger+=1
+            elif row>=max_rows:
+                break
+        row+=1
+    return current_passenger
+
+def fill_window_seats(num_passenger,current_passenger):
+    row = 0
+    while(current_passenger<=num_passenger and row  <max_rows):
+        if row < len(sets_grid[0]):
+            sets_grid[0][row][0] = current_passenger
+            current_passenger += 1
+        if row < len(sets_grid[-1]) and current_passenger<=num_passenger:
+            sets_grid[-1][row][-1] = current_passenger
+            current_passenger += 1   
+        row+=1
+    return current_passenger
+
+
+def fill_center_seats(num_passenger,current_passenger):
+    row = 0
+    while(current_passenger<=num_passenger and row  <max_rows):
+        for i,part in enumerate(sets_grid):
+            if row < len(part) and current_passenger<=num_passenger and len(part[0])>2:
+                for j in range(1,len(part[0])-1):
+                    if current_passenger<=num_passenger:
+                        sets_grid[i][row][j]=current_passenger
+                        current_passenger+=1
+                    else:
+                        break
+            elif row>=max_rows:
+                break
+        row+=1
+    return current_passenger
+
+
+
+
+
+def display(sets_grid):
+    for part in sets_grid:
+        for row in part:
+            print("| ",end="")
+            for col in row:
+                if col != 0:
+                    if col<10:
+                        st = "  " + str(col)
+                    elif col <100:
+                        st = " " + str(col)
+                    else:
+                        st = col
+                    print(st, end=" | ")
+                else:
+                    print("   ", end=" | ")
+            print()
+        print("\n")
+
+next_passenger = fill_aisle_seats(20)
+next_passenger = fill_window_seats(20,next_passenger)
+fill_center_seats(20,next_passenger)
+display(sets_grid)
+            
+
+    
